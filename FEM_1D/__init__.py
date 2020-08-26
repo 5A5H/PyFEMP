cd82bd250072db9888e37dd5b04049b02270c3f5
Sascha Maassen <sascha.maassen@uni-due.de>
2020-08-26 18:14:04 +0200

    new structure for 2d

--- FEM_1D/__init__.py
+++
@@ -1,3 +0,0 @@
-# initialisation file for package
-
-from FEM_1D.FEM_1D_SOLVER import *
---
+++ PyFEM/__init__.py
@@ -0,0 +1,4 @@
+# initialisation file for package
+
+from FEM_1D.FEM_1D_SOLVER import *
+from FEM_1D.meshing import *
