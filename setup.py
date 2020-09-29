9e6787fea20a3c630598de00c1e1dc7843474a5c
Sascha Maassen <sascha.maassen@uni-due.de>
2020-09-29 10:51:49 +0200

    About be shipped

--- setup.py
+++ setup.py
@@ -5,14 +5,22 @@ with open("Readme.md", "r") as fh:
 
 setuptools.setup(
     name="PyFEMP",
-    version="0.0.5",
+    version="0.0.1",
     author="Sascha F. Maassen",
     author_email="sascha.maassen@uni-due.de",
     description="A python FEM solver for educational purposes.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://git.uni-due.de/mechanik/py_fem",
+    url="https://git.uni-due.de/mechanik/py_femp",
     packages=setuptools.find_packages(),
+    project_urls={
+        'Wiki' : 'https://git.uni-due.de/mechanik/py_femp/-/wikis/home'
+    },
+    license='MIT',
+    install_requires=[
+        "numpy",
+        "matplotlib"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
